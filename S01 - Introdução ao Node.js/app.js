71d4ec48f1c4bd68bc7725ab58b7b3ea81755a33
Gustavo <gustavovalenca15@gmail.com>
2023-04-25 16:42:05 -0300

    modulos

---
+++ S01 - Introdução ao Node.js/app.js
@@ -0,0 +1,8 @@
+let calculadora = require("./calculadora.js")
+
+console.log(calculadora.div(10, 20))
+console.log(calculadora.soma(10, 20))
+console.log(calculadora.sub(10, 20))
+console.log(calculadora.mult(10, 20))
+calculadora.versao = 2
+console.log(calculadora.versao)
---
+++ S01 - Introdução ao Node.js/calculadora.js
@@ -0,0 +1,25 @@
+const versao = 1;
+
+function soma(a, b){
+    return a + b
+}
+
+function sub(a, b){
+    return a - b
+}
+
+function mult(a, b){
+    return a * b;
+}
+
+function div(a, b){
+    return a / b;
+}
+
+module.exports = {
+    soma, 
+    sub,
+    mult,
+    div,
+    versao
+};
